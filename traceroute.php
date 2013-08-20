a9a8cde3a3bd8d34f6f6e71130f7459fbc9b16d9
Igor Scheller <igor.scheller@igorshp.de>
2013-08-20 20:05:01 +0200

    Added GeoIP support and help page
    The GeoIP is based on the freegeoip.net RESTful web service API
    The helppage shows you the args and an example

--- traceroute.php
+++ traceroute.php
@@ -2,11 +2,45 @@
 
     define ("SOL_IP", 0);
     define ("IP_TTL", 2);    // On OSX, use '4' instead of '2'.
-
-    $dest_url = "www.google.com";   // Fill in your own URL here, or use $argv[1] to fetch from commandline.
+    
+    echo"\n ============================ PHP-Traceroute ============================\n\n";
+    
+    if (!isset($argv[1]) || $argv[1] == "-h" || $argv[1] == "--help") {
+        // Show the helppage
+        echo "   Usage: sudo php ".$argv[0]." [args] host\n\n";
+        echo "   The available args are:\n";
+        echo "       -h, --help    Show this Help\n";
+        echo "       -g            Show GeoIP lookup for the hosts\n";
+        echo "\n";
+        echo "   Example: sudo php ".$argv[0]." -g github.com\n";
+        
+        echo "\n =======================================================================\n\n";
+        exit;
+    }
+    
+    if ($argv[1] == "-g"){
+        $dest_url = $argv[2];
+    } else {
+        $dest_url = $argv[1];
+    }
+    
     $maximum_hops = 30;
     $port = 33434;  // Standard port that traceroute programs use. Could be anything actually.
-
+    
+    function ip2geo ($host) {
+        global $argv;
+        // Get GeoIP info
+        @$data = file_get_contents('http://freegeoip.net/json/'.$host);
+        if ($data) {
+            $data = json_decode($data);
+            // Return the countrycode, the regionname and the city
+            return $data->country_code.": ".$data->region_name.": ".$data->city;
+        } else {
+            // An error has accourred
+            return "(No geo info found)";
+        }
+    }
+    
     // Get IP from URL
     $dest_addr = gethostbyname ($dest_url);
     print "Tracerouting to destination: $dest_addr\n";
@@ -41,19 +75,26 @@
             socket_recvfrom ($recv_socket, $buf, 512, 0, $recv_addr, $recv_port);
 
             // Calculate the roundtrip time
-            $roundtrip_time = ( microtime(true) – $t1 ) * 1000;
+            $roundtrip_time = ( microtime(true) - $t1 ) * 1000;
 
             // No decent address found, display a * instead
             if (empty ($recv_addr)) {
                 $recv_addr = "*";
                 $recv_name = "*";
             } else {
-                // Otherwise, fetch the hostname for the address found
+                // Otherwise, fetch the hostname and geoinfo for the address found
                 $recv_name = gethostbyaddr ($recv_addr);
+                if ($argv[1] == "-g") {
+                    $recv_geo = ip2geo ($recv_addr);
+                }
             }
 
             // Print statistics
-            printf ("%3d   %-15s  %.3f ms  %s\n", $ttl, $recv_addr,  $roundtrip_time, $recv_name);
+            if ($argv[1] == "-g") {
+                printf ("%3d   %-15s  %.3f ms  %-30s  %s\n", $ttl, $recv_addr,  $roundtrip_time, $recv_geo, $recv_name);
+            } else {
+                printf ("%3d   %-15s  %.3f ms  %s\n", $ttl, $recv_addr,  $roundtrip_time, $recv_name);
+            }
         } else {
             // A timeout has occurred, display a timeout
             printf ("%3d   (timeout)\n", $ttl);
@@ -69,5 +110,7 @@
         // When we have hit our destination, stop the traceroute
         if ($recv_addr == $dest_addr) break;
     }
+    
+    echo "\n ======================================================================\n\n";
 
 ?>
